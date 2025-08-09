aca487cda434213168ce862326d94888ec937dde
Clesnik <clesnik@brrrr.com>
2025-08-09 10:59:18 -0400

    Fix pricing engine client component error

--- src/app/(dashboard)/pricing-engine/page.tsx
+++ src/app/(dashboard)/pricing-engine/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState } from "react"
 import LoanTypeSelection from "./components/loan-type-selection"
 import BorrowerTypeSelection from "./components/borrower-type-selection"
